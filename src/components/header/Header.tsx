658df87a108c3464e43360ff00c432567310d22c
joshuasearle <joshryansearle@gmail.com>
2020-12-05 16:07:08 +1100

    Add slider and button to header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Header: React.FC = () => {
-  return <h1>Header</h1>;
+import TickRateSlider from '../tickRateSlider/TickRateSlider';
+import StartStopButton from '../startStopButton/StartStopButton';
+
+interface HeaderProps {
+  title: string;
+  minTickRate: number;
+  maxTickRate: number;
+  initialTickRate: number;
+}
+
+const Header: React.FC<HeaderProps> = ({
+  title,
+  minTickRate,
+  maxTickRate,
+  initialTickRate,
+}) => {
+  const [currentTickRate, setCurrentTickRate] = useState(initialTickRate);
+  // Initially, the simulation is not running
+  const [running, setRunning] = useState(false);
+
+  const tickChangeHandler = (event: any) => {
+    console.log(event.target.value);
+    setCurrentTickRate(event.target.value);
+  };
+
+  const startStopClickHandler = () => {
+    console.log('start / stop pressed');
+    // Toggle running
+    setRunning(!running);
+  };
+
+  const tickSlider = (
+    <TickRateSlider
+      minTickRate={minTickRate}
+      maxTickRate={maxTickRate}
+      currentTickRate={currentTickRate}
+      onChangeHandler={tickChangeHandler}
+    />
+  );
+
+  return (
+    <header>
+      <h1>{title}</h1>
+      <div>
+        <label>Tick Rate</label>
+        {tickSlider}
+        <StartStopButton
+          running={running}
+          clickHandler={startStopClickHandler}
+        />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
