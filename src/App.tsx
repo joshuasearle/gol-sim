cadda7938fb01b5ac0b6d5f2b594d9eb83cf7d7d
joshuasearle <joshryansearle@gmail.com>
2020-12-05 16:07:43 +1100

    Import constants and feed to Header

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,17 @@ import Header from './components/header/Header';
 import Body from './components/body/Body';
 import Footer from './components/footer/Footer';
 
+import { title, minTickRate, maxTickRate, initialTickRate } from './constants';
+
 const App: React.FC = () => {
   return (
     <>
-      <Header />
+      <Header
+        title={title}
+        minTickRate={minTickRate}
+        maxTickRate={maxTickRate}
+        initialTickRate={initialTickRate}
+      />
       <Body />
       <Footer />
     </>
