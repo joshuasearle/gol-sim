af71c0498b331e9eb82439a3c0341cec0edf3a10
joshuasearle <joshryansearle@gmail.com>
2020-12-06 16:48:49 +1100

    Add body component

--- src/components/body/Body.tsx
+++ src/components/body/Body.tsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import classes from '../../css/classes';
 
-const Body: React.FC = () => {
-  return <h1>Body</h1>;
+import Cell from '../cell/Cell';
+
+interface BodyProps {
+  board: boolean[][];
+}
+
+const Body: React.FC<BodyProps> = ({ board }) => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  console.log(windowWidth);
+
+  useEffect(() => {
+    const windowResizeHandler = () => {
+      setWindowWidth(window.innerWidth);
+    };
+    window.addEventListener('resize', windowResizeHandler);
+    return () => {
+      window.removeEventListener('resize', windowResizeHandler);
+    };
+  });
+
+  const cellWidth = windowWidth / board[0].length;
+
+  return (
+    <table className={classes.board} cellSpacing='0' cellPadding='0'>
+      {board.map(row => (
+        <tr>
+          {row.map(cell => (
+            <Cell alive={cell} cellWidth={cellWidth} />
+          ))}
+        </tr>
+      ))}
+    </table>
+  );
 };
 
 export default Body;
