1b24d0cdddcde6d8a9f23b705a7ecaaef17d463a
joshuasearle <joshryansearle@gmail.com>
2020-12-07 15:50:34 +1100

    Change min / max tick rate

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const title = 'Game of Life Simulator';
 
-export const minTickRate = 10;
-export const maxTickRate = 50;
+export const minTickRate = 1;
+export const maxTickRate = 100;
 export const initialTickRate = (maxTickRate + minTickRate) / 2;
 
 export const initialSpawnChance = 50;
