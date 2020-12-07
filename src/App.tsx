1ac4c3adfb8a917acd3818df4b6b3f93331c0e86
joshuasearle <joshryansearle@gmail.com>
2020-12-07 15:51:15 +1100

    Add board update on tick

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Header from './components/header/Header';
 import Body from './components/body/Body';
@@ -19,11 +19,37 @@ const createEmpty2dArray = (width: number, height: number) => {
   for (let i = 0; i < height; i++) {
     let subArr = [];
     for (let j = 0; j < width; j++) {
-      subArr.push(false);
+      subArr.push(Math.random() > 0.5);
     }
     arr.push(subArr);
   }
   return arr;
+};
+
+const neighbourCount = (board: boolean[][], x: number, y: number) => {
+  let neighbours = 0;
+  for (let i = -1; i <= 1; i++) {
+    for (let j = -1; j <= 1; j++) {
+      if (i === 0 && j == 0) continue;
+      try {
+        neighbours += !!board[x + i][y + j] ? 1 : 0;
+      } catch (e) {
+        continue;
+      }
+    }
+  }
+  return neighbours;
+};
+
+const nextBoard = (board: boolean[][]): boolean[][] => {
+  return board.map((row, x) => {
+    return row.map((alive, y) => {
+      const neibours = neighbourCount(board, x, y);
+      if (alive && (neibours === 2 || neibours === 3)) return true;
+      if (!alive && neibours === 3) return true;
+      return false;
+    });
+  });
 };
 
 const App: React.FC = () => {
@@ -36,6 +62,15 @@ const App: React.FC = () => {
   const [currentSpawnChance, setCurrentSpawnChance] = useState(
     initialSpawnChance
   );
+
+  useEffect(() => {
+    if (running) {
+      setTimeout(() => {
+        const next: boolean[][] = nextBoard(board);
+        setBoard(next);
+      }, 600 / currentTickRate);
+    }
+  }, [running, board]);
 
   // TODO: Fix type
   const tickChangeHandler = (event: any) => {
