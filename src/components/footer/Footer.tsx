a09487240ba3812926c90eb69e2181a390c46022
joshuasearle <joshryansearle@gmail.com>
2020-12-10 16:29:25 +1100

    Create footer component

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import Logo from '../../assets/GitHub-Mark-Light-32px.png';
+import classes from '../../css/classes';
 
 const Footer: React.FC = () => {
-  return <h1>Footer</h1>;
+  return (
+    <div className={classes.footer}>
+      <p>See the source code on GitHub</p>
+      <a href='https://github.com/joshuasearle/gol-sim'>
+        <img src={Logo} alt='GitHub Logo'></img>
+      </a>
+    </div>
+  );
 };
 
 export default Footer;
