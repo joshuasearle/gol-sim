6e61990d89e15dc593627b8ada155ba80c939dc8
joshuasearle <joshryansearle@gmail.com>
2020-12-10 15:41:58 +1100

    Add onclick prop for cell toggle

--- src/components/cell/Cell.tsx
+++ src/components/cell/Cell.tsx
@@ -5,11 +5,13 @@ import { deadColor, aliveColor } from '../../constants';
 interface CellProps {
   alive: boolean;
   cellWidth: number;
+  onClick: any;
 }
 
-const Cell: React.FC<CellProps> = ({ alive, cellWidth }) => {
+const Cell: React.FC<CellProps> = ({ alive, cellWidth, onClick }) => {
   return (
     <td
+      onMouseDown={onClick}
       style={{
         backgroundColor: alive ? aliveColor : deadColor,
         height: cellWidth,
