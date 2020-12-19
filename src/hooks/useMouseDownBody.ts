ed36fb18804dbd3c72b0c03301324f0f137c3515
joshuasearle <joshryansearle@gmail.com>
2020-12-19 17:19:49 +1100

    Custom hook for detecting mouse down in body

---
+++ src/hooks/useMouseDownBody.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+
+// Returns if the mouse went down in the body section of the page
+const useMouseDownBody = () => {
+  const [mouseDown, setMouseDown] = useState(false);
+
+  useEffect(() => {
+    const mouseDownHandler = (e: MouseEvent) => {
+      const header = document.querySelector('.header');
+      const rect = header.getBoundingClientRect();
+      const mouseInHeader = rect.bottom >= e.clientY;
+      if (!mouseInHeader) e.preventDefault();
+      setMouseDown(true);
+    };
+    const mouseUpHandler = (e: MouseEvent) => {
+      setMouseDown(false);
+    };
+    window.addEventListener('mousedown', mouseDownHandler);
+    window.addEventListener('mouseup', mouseUpHandler);
+    return () => {
+      window.removeEventListener('mousedown', mouseDownHandler);
+      window.removeEventListener('mouseup', mouseUpHandler);
+    };
+  });
+
+  return mouseDown;
+};
+
+export default useMouseDownBody;
